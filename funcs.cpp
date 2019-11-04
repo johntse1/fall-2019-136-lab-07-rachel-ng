206d16557642f79cae9ae0a2ddee13420206fd1c
John Tse <jtse63@gmail.com>
2019-11-04 07:58:56 -0500

    commit

--- funcs.cpp
+++ funcs.cpp
@@ -14,7 +14,6 @@ std::string removeLeadingSpaces(std::string line) {
     }
     return line;
 }
-
 int countChar(std::string line, char c) {
     int n = 0;
     for (int i = 0; i < line.size(); i++) {
@@ -23,8 +22,7 @@ int countChar(std::string line, char c) {
         }
     }
     return n;
-}
-
+  }
 std::string tabs (int n) {
     std::string t = "";
     for (int i = 0; i < n; i++) {
@@ -53,18 +51,39 @@ std::string receiveFile (std::string file) {
 std::string indent (std::string code) {
     std::string s, indented;
     int indent = 0;
-    
     std::istringstream lines(code);
-
     while(std::getline(lines, s)) {
+        std::string copy = 
+        if(copy.find("/*")!=std::string::npos){
+          while(copy.find("*/")!=std::string::npos){
+                std::cout << copy << std::endl;
+                std::getline(lines, s);
+          }
+        }
         std::string r = removeLeadingSpaces(s);
         int c = r[0] == '}' ? 1 : 0;
         indented += tabs(indent - c) + r + "\n";
-        indent += countChar(r,'{') - countChar(r,'}'); 
+        indent += countChar(r,'{') - countChar(r,'}');
     }
     return indented;
 }
 
-
-
-
+bool isInQoute(std::string input) {
+    int start = 0;
+    int end = 0;
+    std::string copy = input;
+    if(input.find("\"")!=std::string::npos)
+    {
+      input.find("\"")=start;
+      copy = copy.substr(start,input.length()-1);
+      end = input.find("\"");
+    }
+    for(int i = start; i < end; i++)
+    {
+      if(copy[i]=='{')
+      {
+        return true;
+      }
+    }
+    return false;
+}
